dc77843b0e6705549710bcdf372640d943c5cfa9
delindell <daviselindell@gmail.com>
2020-01-23 19:28:40 -0600

    objects good

--- main.js
+++ main.js
@@ -1,16 +1,74 @@
 console.log('hey yo')
 
-arrOfPets = [
+const arrOfPets = [
     {
-        Image: "https://live.staticflickr.com/5821/23232445753_23b8918c8b_c.jpg",
-        Name: 'Alfred',
-        Color: 'Black',
+        image: "https://live.staticflickr.com/5821/23232445753_23b8918c8b_c.jpg",
+        name: 'Alfred',
+        color: 'Black',
         specialSkill: 'Collector of cuddles',
-        typeOfPet: 'Dog'
+        type: 'Dog'
     },
+    {
+        image: "https://live.staticflickr.com/5821/23232445753_23b8918c8b_c.jpg",
+        name: 'Alfred',
+        color: 'Black',
+        specialSkill: 'Collector of cuddles',
+        type: 'Dog'
+    },
+    {
+        image: "https://live.staticflickr.com/5821/23232445753_23b8918c8b_c.jpg",
+        name: 'Alfred',
+        color: 'Black',
+        specialSkill: 'Collector of cuddles',
+        type: 'Dog'
+    },
+    {
+        image: "https://live.staticflickr.com/5821/23232445753_23b8918c8b_c.jpg",
+        name: 'Alfred',
+        color: 'Black',
+        specialSkill: 'Collector of cuddles',
+        type: 'Dog'
+    },
+    {
+        image: "https://live.staticflickr.com/5821/23232445753_23b8918c8b_c.jpg",
+        name: 'Alfred',
+        color: 'Black',
+        specialSkill: 'Collector of cuddles',
+        type: 'Dog'
+    },
+    {
+        image: "https://live.staticflickr.com/5821/23232445753_23b8918c8b_c.jpg",
+        name: 'Alfred',
+        color: 'Black',
+        specialSkill: 'Collector of cuddles',
+        type: 'Dog'
+    }
     
 
 
 
 ];
+
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+
+};
+
+
+const createPetCards = () => {
+    let domString = '';
+    for (let i = 0; i < arrOfPets.length; i++) {
+        domString += `<div class="cards">`;
+        domString += `<header><h1>${arrOfPets[i].name}</h1></header>`;
+        domString += `<div class="image"><img src="${arrOfPets[i].image} alt="Picture of ${arrOfPets[i].name}></div>`
+        domString += `<section>${arrOfPets[i].color}</section>`;
+        domString += `<p>${arrOfPets[i].specialSkill}</p>`
+        domString += `<footer>${arrOfPets[i].type}</footer>`
+        domString += `</div>`
+        
+    }
+    printToDom('pets', domString); 
+}
 console.log(arrOfPets);
+createPetCards();
